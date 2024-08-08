c7b5f9c150567c1acf284b9d2ff164295749af53
dezhizhang <1541609448@qq.com>
2024-08-08 19:39:24 +0800

    feat:用户服务

---
+++ user/rpc/internal/logic/getuserinfologic.go
@@ -0,0 +1,31 @@
+package logic
+
+import (
+	"context"
+
+	"rpc/internal/svc"
+	"rpc/user"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetUserInfoLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewGetUserInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUserInfoLogic {
+	return &GetUserInfoLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+// GetUserInfo 获取用户信息
+func (l *GetUserInfoLogic) GetUserInfo(in *user.UInfoRequest) (*user.UInfoResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &user.UInfoResponse{}, nil
+}
